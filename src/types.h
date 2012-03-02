779cc1863af6629df8b0541cc72fc284cce103e2
Thomas Bischof <tbischof@mit.edu>
2012-03-02 10:44:17 -0500

    Fixed an error which led to incompatibilities between x86 and x64, regarding the type of time32. Other than that, things seem to be functioning correctly, at first glance. Test the 64-bit and 32-bit versions thoroughly before calling them complete.

--- src/types.h
+++ src/types.h
@@ -7,7 +7,12 @@
 typedef int32_t int32;
 typedef uint32_t uint32;
 typedef int64_t int64;
+#ifdef __i386__
+typedef time_t time32;
+#endif
+#ifdef __x86_64__
 typedef int32_t time32;
+#endif
 
 typedef float float32;
 typedef double float64;
