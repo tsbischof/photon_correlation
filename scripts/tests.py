184cc31fea76a981f0021f8b89f1236fda64fde3
Thomas Bischof <tbischof@mit.edu>
2012-03-02 11:27:53 -0500

    Fill this in with the tests for each executable.

---
+++ scripts/tests.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+
+def test_picoquant():
+    pass
+
+def test_correlate():
+    pass
+
+def test_histogram():
+    pass
+
+if __name__ == "__main__":
+    pass
