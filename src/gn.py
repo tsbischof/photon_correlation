f832c3369029edf39cf18e64fbb08fe83a1a2cb9
Thomas Bischof <tbischofd@mit.edu>
2012-05-10 00:09:16 -0400

    Built a glossary, check for omissions. Also did a tiny bit to start the process of building up gn.py. This should not be too bad.

--- src/gn.py
+++ src/gn.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+import itertools
+
+class Histogram:
+     None
+
+class HistogramBin:
+     None
+
 if __name__ == "__main__":
-     print("hello there")
+     n_channels = 4
+     correlation_order = 2
+     for channels in itertools.combinations_with_replacement(
+          range(n_channels), correlation_order):
+          print(channels)
 
