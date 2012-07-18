57980ae657e92158b5ea75f4ef98b2c780de6b82
Thomas Bischof <tbischof@mit.edu>
2012-07-18 10:48:52 -0400

    Moved things around a bit, to make it easier to use the routines as a library. The next step is to make options handling a bit nicer.

---
+++ src/picoquant_util.h
@@ -0,0 +1,54 @@
+#ifndef PICOQUANT_UTIL_H_
+#define PICOQUANT_UTIL_H_
+
+/* The original code is designed for a 32-bit system, so make sure that the 
+ * bytes line up correctly by forcing the 32-bit memory structure.
+ */
+
+#include <stdio.h>
+#include "types.h"
+#include "options.h"
+
+#define	PQ_SUCCESS	0
+#define PQ_FAILURE -1
+#define	PQ_READ_ERROR	-2
+#define	PQ_MEM_ERROR	-3
+#define PQ_EOF		-4
+#define PQ_MODE_ERROR	-5
+#define PQ_FILE_ERROR	-6
+#define PQ_VERSION_ERROR -7
+
+#pragma pack(4)
+typedef struct {
+	char Ident[16];
+	char FormatVersion[6];
+} pq_header_t;
+
+typedef int (*pq_dispatch_t)(FILE *, FILE *, pq_header_t *, options_t *);
+
+pq_dispatch_t pq_get_dispatch(options_t *options, pq_header_t *pq_header);
+int pq_header_read(FILE *in_stream, pq_header_t *pq_header);
+void pq_header_print(FILE *out_stream, pq_header_t *pq_header);
+
+void pq_print_t2(FILE *out_stream, long long int count,
+		unsigned int channel, 
+		long long int base_time, unsigned int record_time,
+		options_t *options);
+void pq_print_t3(FILE *out_stream, long long int count,
+		unsigned int channel,
+		long long int base_nsync, unsigned int record_nsync,
+		unsigned int record_dtime,
+		options_t *options);
+void pq_print_tttr(FILE *out_stream, long long int count,
+		unsigned int histogram_channel, int n_histogram_channels,
+		long long int base_time, unsigned int record_time,
+		options_t *options);
+void pq_print_interactive(FILE *out_stream, int curve, double left_time, 
+		double right_time, int counts, options_t *options);
+void print_status(long long int count, options_t *options);
+void external_marker(FILE *out_stream, unsigned int marker, 
+		options_t *options);
+void print_resolution(FILE *out_stream, double resolution,
+		options_t *options);
+	
+#endif
