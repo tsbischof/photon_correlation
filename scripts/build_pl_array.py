4bf0980bce720dfb071720be7a8267c0e487438a
Thomas Bischof <tbischof@mit.edu>
2012-08-20 21:33:57 -0400

    Small issues with the messages.

--- scripts/build_pl_array.py
+++ scripts/build_pl_array.py
@@ -14,7 +14,7 @@ def rot90(LoL):
     for i in range(len(LoL[0])):
         yield(map(operator.itemgetter(i), LoL))
 
-def get_filenames(name_base):
+def get_filenames(name_base, as_user=getpass.getuser()):
     root_dir, name = os.path.split(name_base)
 
     if not root_dir:
@@ -22,7 +22,7 @@ def get_filenames(name_base):
 
     logging.debug("Searching directory: {0}".format(root_dir))
 
-    if getpass.getuser() == "rcorrea":
+    if as_user == "rcorrea":
         key = "number"
         gn_parser = re.compile("(?P<name>.+)\.g[0-9]{1}\.(?P<number>[0-9]{8})$")
     else:
@@ -56,21 +56,18 @@ def get_data(filename, strip_autocorrelations):
                 logging.debug("File type not recognized.")
                 break
 
-def gather_histograms(name_base, strip_autocorrelations):
-    root_dir, name = os.path.split(name_base)
-    filenames = list(get_filenames(name_base))
-    logging.debug("Found {0} files to process.".format(len(filenames)))
-
-    dst_filename = "{0}.run".format(name)
-
-    logging.debug("Writing data to {0}".format(dst_filename))
+def gather_histograms(name_base, strip_autocorrelations, as_user):
+    filenames = list(get_filenames(name_base, as_user))
 
     try:
         first = filenames[0]
+        logging.debug("Found {0} files to process.".format(len(filenames)))
     except:
         logging.error("No files to process for {0}.".format(name_base))
         return(False)
     
+    dst_filename = "{0}.run".format(name_base)
+    logging.debug("Writing data to {0}".format(dst_filename))
     with open(dst_filename, "w") as dst_file:
         writer = csv.writer(dst_file)
         for filename in filenames:
@@ -88,18 +85,29 @@ def gather_histograms(name_base, strip_autocorrelations):
         
 
 if __name__ == "__main__":
-##    logging.basicConfig(level=logging.DEBUG)
-    
     usage = "build_pl_array.py name_base"
 
     parser = optparse.OptionParser(usage=usage)
 
-    parser.add_option("-a", "--no-auto", dest="strip_autocorrelations",
-                      help="Remove the autocorrelations from the histograms.",
+    parser.add_option("-a", "--auto", dest="strip_autocorrelations",
+                      help="Keep the autocorrelations in the histograms.",
                       action="store_false", default=True)
+    parser.add_option("-u", "--as-user", dest="as_user",
+                      help="Run the program as though it is as the specified"
+                           "user.",
+                      action="store", type=str, default=getpass.getuser())
+    parser.add_option("-V", "--verbose", dest="verbose",
+                      help="Debug-level messages.",
+                      action="store_true", default=False)
 
     (options, args) = parser.parse_args()
 
+    if options.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.ERROR)
+
     for root_dir in args:
-        gather_histograms(root_dir, options.strip_autocorrelations)
+        gather_histograms(root_dir, options.strip_autocorrelations,
+                          options.as_user)
  
