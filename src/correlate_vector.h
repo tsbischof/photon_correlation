08284cac63256d7444ffc1dbc19ea84613967daf
Thomas Bischof <tbischof@mit.edu>
2012-08-24 15:09:41 -0400

    Forget a header file.

---
+++ src/correlate_vector.h
@@ -0,0 +1,9 @@
+#ifndef CORRELATE_VECTOR_H_
+#define CORRELATE_VECTOR_H_
+
+#include <stdio.h>
+#include "options.h"
+
+int correlate_vector(FILE *in_stream, FILE *out_stream, options_t *options);
+
+#endif
