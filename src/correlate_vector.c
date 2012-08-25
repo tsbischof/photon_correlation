970731bd5f39c5bfb1db86ac780a5106aa6d23c1
Thomas Bischof <tbischof@mit.edu>
2012-08-25 15:23:42 -0400

    Vector correlation will take some time to get working at higher orders, but it may prove worthwhile and perhaps be more efficient when photon numbers get higher.

--- src/correlate_vector.c
+++ src/correlate_vector.c
@@ -8,3 +8,10 @@
 int correlate_vector(FILE *in_stream, FILE *out_stream, options_t *options) {
 	return(0);
 }
+
+/* Perform the implementation by allocating a fixed queue of entries, as needed
+ * to perform all levels of correlation. Have a bunch of pointers to these 
+ * entries, such that, when two valid values are found, the correlation
+ * at that delay can be incremented. Go through this until the stream runs
+ * dry. 
+ */
--- src/correlate_vector_main.c
+++ src/correlate_vector_main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
 	FILE *out_stream = NULL;
 
 	program_options_t program_options = {
-		10,
+		11,
 "This program performs the autocorrelation to nth order of a signal with\n"
 "float intensity. It outputs the raw correlation and the terms required to\n"
 "normalize it.\n",
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
 			OPT_FILE_IN, OPT_FILE_OUT,
 			OPT_ORDER, OPT_PRINT_EVERY,
 			OPT_TIME, OPT_TIME_SCALE, 
-			OPT_APPROXIMATE}};
+			OPT_APPROXIMATE, OPT_TRUE_CORRELATION}};
 
 	
 	result = parse_options(argc, argv, &options, &program_options);
