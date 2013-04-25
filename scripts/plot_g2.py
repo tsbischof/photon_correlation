93fd5682123073a005025279a1df7bf2b0c1ac77
Thomas Bischof <tbischof@mit.edu>
2013-04-25 12:55:04 -0400

    Wrote some quick summaries of the current programs. I will be putting some more time into documentation shortly.

--- scripts/plot_g2.py
+++ scripts/plot_g2.py
@@ -3,35 +3,65 @@
 import csv
 import sys
 import math
+import argparse
 
 import matplotlib.pyplot as plt
 
+def mean(L):
+    return(float(sum(L))/len(L))
+
 def g2_from_stream(stream):
+    result = dict()
     for channel_0, channel_1, bin_left, bin_right, counts in csv.reader(stream):
-        yield((int(channel_0), int(channel_1),
-               float(bin_left), float(bin_right),
-               float(counts)))
+        key = (channel_0, channel_1)
+        if key not in result.keys():
+            result[key] = list()
+
+        result[key].append(((float(bin_left), float(bin_right)), float(counts)))
+
+    return(result)
+
+def plot_g2s(g2s, scale="linear"):
+    if scale == "linear":
+        plotter = plt.plot
+    elif scale == "log":
+        plotter = plt.semilogx
+    else:
+        raise(ValueError("Unknown scale: {0}".format(scale)))
+
+    channels = max(map(max, g2s.keys()))
+
+    plt.clf()
+
+    for index, val in enumerate(g2s.values()):
+        corr, g2 = val
+        c0, c1 = corr
+
+        plt.subplot(channels, channels, index)
+        times = list(map(lambda x: mean(x[0]), g2))
+        counts = list(map(lambda x: x[1], g2))
+   
+        plt.plot(times, counts)
+        plt.title(str(corr))
+        plt.xlabel(r"$\tau$")
+        plt.ylabel(r"g^{(2)}(\tau)")
+
+    plt.show()
 
 if __name__ == "__main__":
-    for filename in sys.argv[1:]:
-        with open(filename) as stream:
-            g2 = list(g2_from_stream(stream))
+    parser = argparse.ArgumentParser(description="Plot a g2.")
 
-            correlations = set(map(lambda x: (x[0], x[1]),
-                                   g2))
-            channels = int(math.sqrt(len(correlations)))
+    parser.add_argument("--scale", default="linear", type=str,
+                        help="Scale for the time axis display, either "
+                             "linear or log.")
 
-            plt.clf()
-            for index, correlation in enumerate(sorted(correlations)):
-                plt.subplot(channels, channels,index)
-                my_correlation = list(
-                    filter(lambda x: (x[0], x[1]) == correlation,
-                           g2))
-                my_correlation.sort()
+    parser.add_argument("files", type=str, nargs="*",
+                        help="Filenames containing g2 data to plot.")
 
-                plt.plot(map(lambda x: (x[2]+x[3])/2, my_correlation),
-                            map(lambda x: x[4], my_correlation))
-                plt.title(str(correlation))
-                plt.xlabel(r"$\tau$")
-                plt.ylabel(r"$g^{(2)}(\tau)$")
-            plt.show()
+    args = parser.parse_args()
+
+    for filename in args.files:
+        with open(filename) as stream_in:
+            g2s = g2_from_stream(stream_in)
+
+        plot_g2s(g2s, scale=args.scale)
