a12d76eae4ee5743fdee2f249f01c02e9a3d29d2
Thomas Bischof <tbischof@mit.edu>
2013-10-17 18:31:43 -0400

    Added a brief help message to photon_intensity_correlate

--- src/photon_intensity_correlate_main.c
+++ src/photon_intensity_correlate_main.c
@@ -4,8 +4,22 @@
 
 int main(int argc, char *argv[]) {
 	program_options_t program_options = {
-		"",
-
+"This program performs an intensity correlation of photon arrival time data.\n"
+"The algorithm used mimics that of standard multi-tau hardware correlators,\n"
+"which perform linear-binned intensity correlations at logarithmically-\n"
+"increasing bin widths. For example, with three registers and a binning of\n"
+"three, the effective bins look like:\n"
+" |0|1|2|\n"
+" |     |  5  |  8  | \n"
+" |                 |       13        |       22        |\n"
+"In this picture, each row forms some number of correlation bins by a normal\n"
+"linear correlation of the blank bin with the others. By updating the signal\n"
+"by pushing existing values into the larger bins as the small bins are\n"
+"filled, we obtain an effective logarithmic scaling, up to some number of\n"
+"rescales (the depth). This way, each time bin for the correlation is only\n"
+"sampled as often as necessary, ensuring that reasonable signal-to-noise\n"
+"can be obtained at all time scales without requiring significant duplication\n"
+"of effort.\n",
 		{OPT_VERBOSE, OPT_HELP, OPT_VERSION,
 			OPT_FILE_IN, OPT_FILE_OUT,
 			OPT_CHANNELS, OPT_ORDER,
