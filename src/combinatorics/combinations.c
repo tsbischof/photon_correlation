b179993f9a9c06efad40f0a0f1c761a22a028cc3
Thomas Bischof <tbischof@mit.edu>
2015-03-14 13:59:04 -0400

    Updated the year in the copyright

--- src/combinatorics/combinations.c
+++ src/combinatorics/combinations.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2014, Thomas Bischof
+ * Copyright (c) 2011-2015, Thomas Bischof
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without 
